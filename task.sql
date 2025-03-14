c50eaba1ad4592654c9c2ac6c76a11c617ab3da3
Volodymyrdv <vovakryt123453@gmail.com>
2025-03-14 11:39:30 +0200

    Fixed hardcore for OrderId

--- task.sql
+++ task.sql
@@ -4,10 +4,12 @@ USE ShopDB;
 INSERT INTO Orders (CustomerID, Date)
 VALUES (1, '2023-01-01');
 
+SET @OrderID = LAST_INSERT_ID();
+
 -- Start the transaction 
 START TRANSACTION; 
 INSERT INTO OrderItems (OrderID, ProductID, Count)
-VALUES (1, 1, 1);
+VALUES (@OrderID, 1, 1);
 UPDATE Products SET WarehouseAmount = WarehouseAmount - 1
 WHERE Name = 'AwersomeProduct';
 
